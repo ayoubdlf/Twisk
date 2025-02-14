1d20d0bc6f3d2e73c9b61d2dc1fdb194d8b14e44
KABOUCHE sara <sara.kabouche9@etu.univ-lorraine.fr>
2025-02-14 10:38:36 +0100

    Récuperation du code c pour mac et linux

---
+++ src/main/ressources/codeC/client.c
@@ -0,0 +1,12 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+void simulation (int ids){
+    //fonction qui simule le parcours d'un client dans le graphe
+    //faire appel aux :
+    /*
+     *void delai(int temps, int delta) // 0 < delta < temps < 100
+     *void entrer(int etape)
+     *void transfert(int source, int destination)
+    */
+}

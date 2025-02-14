0d6c9147e24eba212470bccf8f8122c3d8c38d4a
KABOUCHE sara <sara.kabouche9@etu.univ-lorraine.fr>
2025-02-14 10:49:36 +0100

    Création d'un monde minimaliste

--- src/main/ressources/codeC/client.c
+++ src/main/ressources/codeC/client.c
@@ -1,7 +1,28 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<time.h>
+
+#include "def.h"
+
+#define sasEntree 0
+#define sasSortie 1
+#define guichet 2
+#define num_sum_guichet 1
+#define activite 3
+
 
 void simulation (int ids){
+
+    entrer(sasEntree);
+    delai(6,3);
+    transfert(sasEntree , guichet);
+
+    p(ids , num_sem_guichet);
+        transfert(guichet , activitee);
+        delai(8,2);
+    V(ids , num_sem_guichet);
+
+    treansfer(activite , sasSortie);
     //fonction qui simule le parcours d'un client dans le graphe
     //faire appel aux :
     /*

68b62a1205cb0a6e24859761f23c73bdddcc0318
KABOUCHE sara <sara.kabouche9@etu.univ-lorraine.fr>
2025-02-14 12:19:31 +0100

    Mise a jours du makefile et programme de simulation

--- src/main/c/main.c
+++ src/main/c/main.c
@@ -11,6 +11,16 @@ int main(int argc, char** argv) {
     int tabJetonsGuichet[] = { 1 };
     int* simulation        = start_simulation(NB_ETAPES, NB_GUICHETS, NB_CLIENTS, tabJetonsGuichet);
 
+    int* position = ou_sont_les_clients(NB_ETAPES , NB_CLIENTS);
+
+    for(int i = 0 ; i < NB_ETAPES ; i++) {
+        printf("Etape: %d", position[i]);
+        for(int j = 0 ; j < (int)position[0] ; j++) {
+            printf("%d, ", position[i+j+1]);
+        }
+        printf("\n");
+    }
+
     nettoyage();
 
     return EXIT_SUCCESS;

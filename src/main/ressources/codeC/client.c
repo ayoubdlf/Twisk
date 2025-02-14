6c8307763e6de0c967ff6bfb2a02d5fc9740e8f9
KABOUCHE sara <sara.kabouche9@etu.univ-lorraine.fr>
2025-02-14 11:10:19 +0100

    correction monde

--- src/main/ressources/codeC/client.c
+++ src/main/ressources/codeC/client.c
@@ -1,13 +1,13 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 
 #include "def.h"
 
 #define sasEntree 0
 #define sasSortie 1
 #define guichet 2
-#define num_sum_guichet 1
+#define num_sem_guichet 1
 #define activite 3
 
 
@@ -17,18 +17,10 @@ void simulation (int ids){
     delai(6,3);
     transfert(sasEntree , guichet);
 
-    p(ids , num_sem_guichet);
-        transfert(guichet , activitee);
+    P(ids , num_sem_guichet);
+        transfert(guichet , activite);
         delai(8,2);
     V(ids , num_sem_guichet);
 
-    treansfer(activite , sasSortie);
-    //fonction qui simule le parcours d'un client dans le graphe
-    //faire appel aux :
-    /*
-     *void delai(int temps, int delta) // 0 < delta < temps < 100
-     *void entrer(int etape)
-     *void transfert(int source, int destination)
-    */
-
+    transfert(activite , sasSortie);
 }
